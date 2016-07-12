03ecedea42e008ee76e2482122a83adaba0e1950
Andrew Naidyonov <dront@itoolabs.com>
2016-07-12 22:32:47 +0300

    TinyXML2 cmake module

---
+++ cmake/FindTinyXML2.cmake
@@ -0,0 +1,46 @@
+find_path(TINYXML_INCLUDE_DIR
+    NAMES
+    tinyxml2.h
+    HINTS
+    "${CMAKE_SOURCE_DIR}/opt/include"
+    PATH_SUFFIXES
+    include
+    PATHS
+    "${CMAKE_SOURCE_DIR}/opt/include"
+    /usr/include
+    /opt/local/include
+    /usr/local/include
+    NO_DEFAULT_PATH
+)
+
+find_library(TINYXML_LIBRARY
+    NAMES tinyxml2
+    HINTS
+    "${CMAKE_SOURCE_DIR}/opt/lib"
+    PATH_SUFFIXES
+    lib
+    PATHS
+    "${CMAKE_SOURCE_DIR}/opt/lib"
+    /usr/local/lib
+    /opt/local/lib
+    /usr/lib
+    NO_DEFAULT_PATH
+)
+
+if (TINYXML_INCLUDE_DIR AND TINYXML_LIBRARY)
+  set(TINYXML_FOUND TRUE)
+  set( TINYXML_LIBRARIES ${TINYXML_LIBRARY} )
+else ()
+  set(TINYXML_FOUND FALSE)
+  set( TINYXML_LIBRARIES )
+endif ()
+
+if (TINYXML_FOUND)
+  message(STATUS "Found TinyXML: ${TINYXML_LIBRARY}")
+  mark_as_advanced(
+    TINYXML_LIBRARY
+    TINYXML_INCLUDE_DIR
+  )
+else ()
+  message(STATUS "Not Found TinyXML library - trying to build on next run")
+endif ()
