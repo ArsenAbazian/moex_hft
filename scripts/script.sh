d5476dea7e7e0cbf2b88cd1cc67edb66a34af7b7
arsen <arsenabazyan@gmail.com>
2017-03-20 17:52:47 +0300

    modify script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-route add -net 1.1.1.0 netmask 255.255.255.0 gateway         1.1.4.248
-route add -net 239.192.0.0 netmask 255.255.0.0 gateway       1.1.4.248
-route add -net 10.50.129.200 netmask 255.255.255.255 gateway 1.1.4.248
-route add -net 10.50.129.90 netmask 255.255.255.255 gateway  1.1.4.248
-route add -net 1.1.7.200 netmask 255.255.255.255 gateway     1.1.4.248
+moex_address=$1
+echo "adding moex address = $moex_address"
+route add -net 1.1.1.0 netmask 255.255.255.0 gateway         $moex_address
+route add -net 239.192.0.0 netmask 255.255.0.0 gateway       $moex_address
+route add -net 10.50.129.200 netmask 255.255.255.255 gateway $moex_address
+route add -net 10.50.129.90 netmask 255.255.255.255 gateway  $moex_address
+route add -net 1.1.7.200 netmask 255.255.255.255 gateway     $moex_address
 
