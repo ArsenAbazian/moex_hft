c5f7512a07ddb262bc318b90b9f34eb555b6e40c
arsen <arsenabazyan@gmail.com>
2017-03-20 22:54:29 +0300

    1. modifing script.sh to get info from ifconfig. There is no need to change content fo script.sh file anymore

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 info=$(/sbin/ifconfig)
-echo "display $info"
-moex_address="1.1.4.24"
-echo "adding moex address = $moex_address"
+start=${info#*ppp0}
+start=${start#*addr:}
+moex_address=${start%% *}
+echo "MOEX address = $moex_address"
 route add -net 1.1.1.0 netmask 255.255.255.0 gateway         $moex_address
 route add -net 239.192.0.0 netmask 255.255.0.0 gateway       $moex_address
 route add -net 10.50.129.200 netmask 255.255.255.255 gateway $moex_address
